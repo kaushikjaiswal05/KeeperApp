b5d3011401c47cd84fa4de04e942c1c8fc9b7865
kaushikjaiswal05 <kaushikjaiswal024@gmail.com>
2023-05-27 18:36:07 +0530

    firstCommit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="outlined">Outlined</Button>
+        <Button variant="outlined">Outlined vvv</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
